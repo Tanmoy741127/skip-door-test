1c2006d14c34ef5f4468e3d8b64fb9d061355f37
Tanmoy Sarkar <ts741127@gmail.com>
2022-07-12 03:12:07 +0530

    update

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, request
 from urllib.parse import urlparse
 from urllib.parse import parse_qs
 import requests
-
+import os
 
 app = Flask(__name__)
 
@@ -56,3 +56,7 @@ def submitSkipRequest():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT", 5000))
+#     app.run(host='0.0.0.0', port=port)
